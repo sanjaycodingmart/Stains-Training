363a4900a1ddec13cb2ad372d6aac6c329a1af24
stainsmjohnson <grahamstains1999@gmail.com>
2019-12-25 19:42:54 +0530

    working api which fetch gif from giffy and also can search , added scroll event listener

--- API Access/script.js
+++ API Access/script.js
@@ -1,16 +1,53 @@
-let frst=setInterval(async function fet(){
-    const a = await fetch('https://api.wheretheiss.at/v1/satellites/25544');
-    console.log(a);
-    const data = await a.json();
-    console.log(data);
-    latitude =data.latitude ;
-    longitude =data.longitude;
-    console.log(latitude , longitude);
-    document.querySelector('#lat').textContent=latitude;
-    const lon = document.querySelector('#lon').textContent=longitude;
-},30);
+const api = 'M9hikonu4VTGugShMmjIglQMF40v33Tk';
+let search = '';
+count = 0;
+let url = 'http://api.giphy.com/v1/gifs/trending?api_key='+api+'&offset='+count;
 
-let latitude,longitude;
-fet().catch(error => {
+//fetch function for gif api
+async function getGIF(url){
+    const response = await fetch(url);
+    const json = await response.json();
+
+    json.data.forEach(imgid => {
+        let container = document.querySelector('#container');
+        let img = document.createElement('IMG');
+        img.src=imgid.images.preview_gif.url;
+        img.height = 300;
+        container.appendChild(img);
+    });
+}
+
+//scrolling event
+document.addEventListener('scroll', () => {
+    let total = document.documentElement.scrollHeight - window.innerHeight;
+    let scrolV = window.scrollY;
+
+    
+
+    if(scrolV==total){
+        count+=25;
+        if(search==''){
+            url = 'http://api.giphy.com/v1/gifs/trending?api_key='+api+'&offset='+count;
+        }
+        else{
+            url = 'http://api.giphy.com/v1/gifs/search?q='+search+'&api_key='+api+'&offset='+count;
+        }
+        getGIF(url);   
+    }
+});
+
+
+
+//initial calling
+getGIF(url).catch(error => {
     console.error(error);
 });
+
+
+//search for gif
+function srch(){
+    search = document.getElementById('input').value;
+    document.querySelector('#container').innerHTML = "";
+    url = 'http://api.giphy.com/v1/gifs/search?q='+search+'&api_key='+api+'&offset='+count;
+    getGIF(url);
+}
