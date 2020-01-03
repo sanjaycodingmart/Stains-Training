a29d632023712cb54eb14b0de84679977313d550
stainsmjohnson <grahamstains1999@gmail.com>
2020-01-03 15:27:20 +0530

    react first

---
+++ Stripe Payment/Checkout/checkout.js
@@ -0,0 +1,4 @@
+// Set your publishable key: remember to change this to your live publishable key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+var stripe = Stripe('pk_test_7yS5fRp2iLYUkkCPf5Q23Pev0064EMeGWs');
+var elements = stripe.elements();
---
+++ Stripe Payment/script.js
@@ -0,0 +1,2 @@
+public_key ="pk_test_7yS5fRp2iLYUkkCPf5Q23Pev0064EMeGWs";
+private_key ="sk_test_n3GAIK81PAuEc8yT2CvAsAxN004ON6qGD7";
